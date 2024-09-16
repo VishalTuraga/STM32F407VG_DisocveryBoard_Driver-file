0d4e47161e56d2c5b5b25f3511232e727dd6adad
VishalTuraga <vishalturaga@gmail.com>
2024-09-16 21:34:57 +0530

    Added the configuration structure and the handle structure for i2c in the .h file

--- drivers/Inc/stm32f407xx_i2c.h
+++ drivers/Inc/stm32f407xx_i2c.h
@@ -10,4 +10,21 @@
 
 #include "stm32f407xx.h"
 
+/*
+ * Configuration structure for the I2C peripheral
+ */
+typedef struct
+{
+	uint32_t 	I2C_SCLSpeed;
+	uint8_t 	I2C_DeviceAddress;
+	uint8_t 	I2C_ACKControl;
+	uint16_t	I2C_FMDutyCycle;
+}I2C_Config_t;
+
+typedef struct
+{
+	I2C_Config_t I2C_Config;
+	I2C_RegDef_t *pI2Cx;
+}I2C_Handle_t;
+
 #endif /* INC_STM32F407XX_I2C_H_ */
