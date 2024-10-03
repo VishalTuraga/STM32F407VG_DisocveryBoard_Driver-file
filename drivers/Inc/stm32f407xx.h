d9b8a4dbfffbcddf9eb603f5a44ffbede0d33551
VishalTuraga <vishalturaga@gmail.com>
2024-10-03 21:55:00 +0530

    Added USART register bit definition macros in MCU specific header file

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -675,6 +675,81 @@ typedef struct
 #define I2C_CCR_DUTY					14
 #define I2C_CCR_FS						15
 
+
+/*
+ * USART SR bit position definitions
+ */
+#define USART_SR_CTS					9
+#define USART_SR_LBD					8
+#define USART_SR_TXE					7
+#define USART_SR_TC						6
+#define USART_SR_RXNE					5
+#define USART_SR_IDLE					4
+#define USART_SR_ORE					3
+#define USART_SR_NF						2
+#define USART_SR_FE						1
+#define USART_SR_PE						0
+
+/*
+ * USART BRR bit position definitions
+ */
+#define USART_BRR_DIV_FRACTION			0
+#define USART_BRR_DIV_MANTISSA			4
+
+/*
+ * USART CR1 bit position definitions
+ */
+#define USART_CR1_OVER8					15
+#define USART_CR1_UE					13
+#define USART_CR1_M						12
+#define USART_CR1_WAKE					11
+#define USART_CR1_PCE					10
+#define USART_CR1_PS					9
+#define USART_CR1_PEIE					8
+#define USART_CR1_TXEIE					7
+#define USART_CR1_TCIE					6
+#define USART_CR1_RXNEIE				5
+#define USART_CR1_IDLEIE				4
+#define USART_CR1_TE					3
+#define USART_CR1_RE					2
+#define USART_CR1_RWU					1
+#define USART_CR1_SBK					0
+
+/*
+ * USART CR2 bit position definitions
+ */
+#define USART_CR2_LINEN					14
+#define USART_CR2_STOP					13
+#define USART_CR2_CLKEN					11
+#define USART_CR2_CPOL					10
+#define USART_CR2_CPHA					9
+#define USART_CR2_LBCL					8
+#define USART_CR2_LBDIE					6
+#define USART_CR2_LBDL					5
+#define USART_CR2_ADD					0
+
+/*
+ * USART CR3 bit position definitions
+ */
+#define USART_CR3_ONEBIT				11
+#define USART_CR3_CTSIE					10
+#define USART_CR3_CTSE					9
+#define USART_CR3_RTSE					8
+#define USART_CR3_DMAT					7
+#define USART_CR3_DMAR					6
+#define USART_CR3_SCEN					5
+#define USART_CR3_NACK					4
+#define USART_CR3_HDSEL					3
+#define USART_CR3_IRLP					2
+#define USART_CR3_IREN					1
+#define USART_CR3_EIE					0
+
+/*
+ * USART GTPR bit position definitions
+ */
+#define USART_GTPR_PSC					0
+#define USART_GTPR_GT					8
+
 /*
  * Generic functions
  */
