59a7e0c8c3aed8c0845263e50bf673488490e7b7
VishalTuraga <vishalturaga@gmail.com>
2024-11-14 19:26:26 +0530

    Implemented USART_SetBaudRate

--- drivers/Src/stm32f407xx_usart.c
+++ drivers/Src/stm32f407xx_usart.c
@@ -533,35 +533,35 @@ void USART_SetBaudRate(USART_RegDef_t *pUSARTx, uint32_t BaudRate)
 	}
 
 	//Check for OVER8 configuration bit
-	if(pUSARTx->CR1 & (1 << TODO))
+	if(pUSARTx->CR1 & (1 << USART_CR1_OVER8))
 	{
 		//OVER8 = 1 , over sampling by 8
 		usartdiv = ((25 * PCLKx) / (2 *BaudRate));
 	}else
 	{
 		//over sampling by 16
-		TODO
+		usartdiv = ((25 * PCLKx) / (4 * BaudRate));
 	}
 
 	//Calculate the Mantissa part
-	M_part = TODO/100;
+	M_part = usartdiv/100;
 
 	//Place the Mantissa part in appropriate bit position . refer USART_BRR
 	tempreg |= M_part << 4;
 
 	//Extract the fraction part
-	F_part = (usartdiv - (TODO * 100));
+	F_part = (usartdiv - (M_part * 100));
 
 	//Calculate the final fractional
 	if(pUSARTx->CR1 & ( 1 << USART_CR1_OVER8))
 	{
 		//OVER8 = 1 , over sampling by 8
-		F_part = ((( F_part * TODO)+ 50) / 100)& ((uint8_t)0x07);
+		F_part = ((( F_part * 8)+ 50) / 100)& ((uint8_t)0x07);
 
 	}else
 	{
 		//over sampling by 16
-		F_part = ((( F_part * TODO)+ 50) / 100) & ((uint8_t)0x0F);
+		F_part = ((( F_part * 16)+ 50) / 100) & ((uint8_t)0x0F);
 
 	}
 
@@ -569,7 +569,7 @@ void USART_SetBaudRate(USART_RegDef_t *pUSARTx, uint32_t BaudRate)
 	tempreg |= F_part;
 
 	//copy the value of tempreg in to BRR register
-	pUSARTx->TODO = tempreg;
+	pUSARTx->BRR = tempreg;
 }
 
 /*
